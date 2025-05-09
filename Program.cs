c919a79403104795f7f4e6ddd65b474bc899fc0d
mario rojas <mario.rojaset12d1@gmail.com>
2025-05-09 20:28:29 -0300

    generador de tabla

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.WriteLine("Ingerese un numero(entero): ");
+int numero = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while( i < 13){
+   
+    Console.WriteLine(numero + "x" + i + "="+(numero * i));
+    i++;
+}
